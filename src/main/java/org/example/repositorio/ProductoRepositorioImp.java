37f27234f16771536ff2728060f5c592406c1d24
ElShaman <cbuitragoo.2023@gmail.com>
2023-03-08 15:14:14 -0500

    crud terminado listo para pruebas

--- src/main/java/org/example/repositorio/ProductoRepositorioImp.java
+++ src/main/java/org/example/repositorio/ProductoRepositorioImp.java
@@ -58,11 +58,34 @@ public class ProductoRepositorioImp implements Repositorio<Producto> {
 
     @Override
     public void guardar(Producto producto) {
+        String sql = null;
+        if (producto.getId() !=null && producto.getId()>0) {
+            sql = "UPDATE productos SET nombre=?, precio =? WHERE id=?";
+        }else{
+            sql = "INSERT INTO PRODUCTOS(nombre, precio, fecha_registro) values(?,?,?)";
+        }
+        try(PreparedStatement s = getConnection().prepareStatement(sql)){
+            s.setString(1, producto.getNombre());
+            s.setLong(2, producto.getPrecio());
+            if(producto.getId() !=null && producto.getId()>0){
+                s.setLong(3, producto.getId());
+            }else{
 
+            }
+            s.setDate(3, new Date(producto.getFechaRegistro().getTime()));
+            s.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void eliminar(Long id) {
-
+        try(PreparedStatement s= getConnection().prepareStatement("DELETE FROM PRODUCTOS WHERE id=?")) {
+            s.setLong(1, id);
+            s.executeUpdate();
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
     }
 }
